575e2bfcff9e2697570cca65010f77c0b2a37ebc
SeedPaths-User <lessdismissive@email.com>
2014-09-29 12:55:34 -0600

    Done, could look nicer

---
+++ Portfolio/Portfolio/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new Models.ContactInformation());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Models.ContactInformation contactForm)
+        {
+            Models.ContactInfoEntities db = new Models.ContactInfoEntities();
+            db.ContactInformations.Add(contactForm);
+            db.SaveChanges();
+            return RedirectToAction("ThankYou", "Contact");
+        }
+
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Portfolio/Portfolio/Models/ContactInformationValidation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+    [MetadataType(typeof(ContactInformationValidation))]
+    public partial class ContactInformation
+    {
+    }
+
+    public class ContactInformationValidation
+    {
+        [Required, MaxLength(200)]
+        public string FirstName { get; set; }
+        [Required, MaxLength(200)]
+        public string LastName { get; set; }
+        [Required, MaxLength(200)]
+        public string Email { get; set; }
+        [Required, MaxLength(20)]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Comment { get; set; }
+    }
+}
